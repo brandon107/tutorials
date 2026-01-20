0e2d12b238611ebd6f6922551666d05239dffcc8
brandon107 <brandon@airdynamics.com.sg>
2026-01-20 17:39:31 +0800

    Chapter 6 'Domain' section

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,8 @@
     'name': 'Estate',
     'application': True,
     'data': [
-        'security/ir.model.access.csv'
-        'views/estate_property_views.xml'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,28 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class Property(models.Model):
     _name = "estate.property"
     _description = "Property model for the estate module."
 
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('oreceived', 'Offer Received'), ('oaccepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new'
+    )
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today() + relativedelta(months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
