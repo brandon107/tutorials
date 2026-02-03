78c71e07e0e5c9f8c644c3f94d787fbfdf3497db
brandon107 <brandon@airdynamics.com.sg>
2026-02-03 11:08:39 +0800

    Ch8 complete

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -13,6 +13,21 @@ class Property(models.Model):
         for record in self:
             record.total_area = record.living_area + record.garden_area
 
+    garden = fields.Boolean()
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north','North'), ('south','South'), ('east','East'), ('west','West')]
+    )
+    
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ""
+
     active = fields.Boolean('Active', default=True)
     state = fields.Selection(
         string='State',
@@ -20,41 +35,36 @@ class Property(models.Model):
         required=True,
         copy=False,
         default='new'
-        )
+    )
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
         copy=False,
         default=lambda self: fields.Date.today() + relativedelta(months=3)
-        )
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     facades = fields.Integer()
     garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_orientation = fields.Selection(
-        string='Garden Orientation',
-        selection=[('north','North'), ('south','South'), ('east','East'), ('west','West')]
-        )
 
     buyer_id = fields.Many2one(
         "res.partner",
         string="Buyer",
         copy=False
-        )
+    )
     seller_id = fields.Many2one(
         "res.users",
         string="Salesman",
         default=lambda self: self._uid
-        )
+    )
     property_type_id = fields.Many2one(
         "estate.property.type", string="Property Type"
-        )
+    )
     tag_ids=fields.Many2many(
         "estate.property.tag", string="Property Tag"
-        )
+    )
     offer_ids=fields.One2many(
         "estate.property.offer", "property_id", string="Property Offer"
     )
