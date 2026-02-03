316899e4d14282ba476d6a8f43456481a21e965a
brandon107 <brandon@airdynamics.com.sg>
2026-02-03 16:52:57 +0800

    Ch9 WIP

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError
 
 class Property(models.Model):
     _name = "estate.property"
@@ -30,7 +31,7 @@ class Property(models.Model):
 
     active = fields.Boolean('Active', default=True)
     state = fields.Selection(
-        string='State',
+        string='Status',
         selection=[('new', 'New'), ('oreceived', 'Offer Received'), ('oaccepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
         required=True,
         copy=False,
@@ -77,4 +78,20 @@ class Property(models.Model):
                 record.best_price = max(record.offer_ids.mapped('price'))
             else:
                 record.best_price = 0
+
+    def action_sold(self):
+        for record in self:
+            if record.state == 'cancelled':
+                raise UserError("Cancelled properties cannot be sold.")
+            else:
+                record.state = 'sold'
+        return True
+    
+    def action_cancel(self):
+        for record in self:
+            if record.state == 'sold':
+                raise UserError("Sold properties cannot be cancelled.")
+            else:
+                record.state = 'cancelled'
+        return True
 
